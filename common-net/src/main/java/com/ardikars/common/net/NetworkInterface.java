84d43a91a0bd011ee38cc4d9c784dc7bae81eb6b
Ardika Rommy Sanjaya <ardikars@gmail.com>
2018-08-17 15:53:35 +0700

    Replace Collections.EMPTY_SET with new HashSet()

--- common-net/src/main/java/com/ardikars/common/net/NetworkInterface.java
+++ common-net/src/main/java/com/ardikars/common/net/NetworkInterface.java
@@ -218,7 +218,7 @@ public class NetworkInterface {
                     .loopback(networkInterface.isLoopback())
                     .up(networkInterface.isUp())
                     .parent(null)
-                    .childs(Collections.EMPTY_SET)
+                    .childs(new HashSet())
                     .build();
             Enumeration<java.net.NetworkInterface> childs = networkInterface.getSubInterfaces();
             while (childs.hasMoreElements()) {
@@ -244,7 +244,7 @@ public class NetworkInterface {
                         .loopback(childNetworkInterface.isLoopback())
                         .up(childNetworkInterface.isUp())
                         .parent(parent)
-                        .childs(Collections.EMPTY_SET)
+                        .childs(new HashSet())
                         .build();
                 parent.addChild(child);
             }
