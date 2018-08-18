c05655a1653f1fa395590c4052409102c18e9a9a
Ardika Rommy Sanjaya <ardikars@gmail.com>
2018-08-18 11:35:09 +0700

    Add incubating annotation

--- common-annotation/src/main/java/com/ardikars/common/annotation/Incubating.java
+++ common-annotation/src/main/java/com/ardikars/common/annotation/Incubating.java
@@ -16,17 +16,16 @@
 
 package com.ardikars.common.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
- * Annotate helper class.
- * @author <a href="mailto:contact@ardikars.com">Ardika Rommy Sanjaya</a>
- * @since 1.0.0
+ * Indicates that a feature is incubating. This means that the feature is currently a work-in-progress and may
+ * change at any time.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-@Incubating
-public @interface Helper {
+public @interface Incubating {
 
 }
--- common-annotation/src/main/java/com/ardikars/common/annotation/Mutable.java
+++ common-annotation/src/main/java/com/ardikars/common/annotation/Mutable.java
@@ -16,16 +16,14 @@
 
 package com.ardikars.common.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * Annotate non thread-safe class.
  * @author <a href="mailto:contact@ardikars.com">Ardika Rommy Sanjaya</a>
  * @since 1.0.0
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Mutable {
