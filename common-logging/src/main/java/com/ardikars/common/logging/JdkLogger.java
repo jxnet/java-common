ff8a3cb4b12ddc764985e092adbf0bbe38cf0976
Ardika Rommy Sanjaya <ardikars@gmail.com>
2018-12-28 15:07:23 +0700

    Fix jdk logger formatting message

--- common-logging/src/main/java/com/ardikars/common/logging/JdkLogger.java
+++ common-logging/src/main/java/com/ardikars/common/logging/JdkLogger.java
@@ -1,6 +1,5 @@
 package com.ardikars.common.logging;
 
-import java.util.Formatter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
@@ -15,7 +14,6 @@ class JdkLogger extends AbstractLogger {
     private static final String SUPER = AbstractLogger.class.getName();
 
     private final transient Logger logger;
-    private final Formatter formatter = new Formatter();
 
     JdkLogger(Logger logger) {
         super(logger.getName());
@@ -45,128 +43,124 @@ class JdkLogger extends AbstractLogger {
     @Override
     public void debug(String format, Object arg1) {
         if (logger.isLoggable(Level.ALL)) {
-            String msg = formatter.format(format, arg1).toString();
-            log(SELF, Level.ALL, msg, null);
+            FormattingTuple tuple = MessageFormatter.format(format, arg1);
+            log(SELF, Level.ALL, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void debug(String format, Object arg1, Object arg2) {
         if (logger.isLoggable(Level.ALL)) {
-            String msg = formatter.format(format, arg1, arg2).toString();
-            log(SELF, Level.ALL, msg, null);
+            FormattingTuple tuple = MessageFormatter.format(format, arg1, arg2);
+            log(SELF, Level.ALL, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void debug(String format, Object... arguments) {
         if (logger.isLoggable(Level.ALL)) {
-            String msg = formatter.format(format, arguments).toString();
-            log(SELF, Level.ALL, msg, null);
+            FormattingTuple tuple = MessageFormatter.arrayFormat(format, arguments);
+            log(SELF, Level.ALL, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void debug(String format, Throwable throwable) {
         if (logger.isLoggable(Level.ALL)) {
-            String msg = formatter.format(format, throwable).toString();
-            log(SELF, Level.ALL, msg, throwable);
+            log(SELF, Level.ALL, format, throwable);
         }
     }
 
     @Override
     public void info(String format, Object obj1) {
         if (logger.isLoggable(Level.INFO)) {
-            String msg = formatter.format(format, obj1).toString();
-            log(SELF, Level.INFO, msg, null);
+            FormattingTuple tuple = MessageFormatter.format(format, obj1);
+            log(SELF, Level.INFO, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void info(String format, Object obj1, Object obj2) {
         if (logger.isLoggable(Level.INFO)) {
-            String msg = formatter.format(format, obj1, obj2).toString();
-            log(SELF, Level.INFO, msg, null);
+            FormattingTuple tuple = MessageFormatter.format(format, obj1, obj2);
+            log(SELF, Level.INFO, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void info(String format, Object... arguments) {
         if (logger.isLoggable(Level.INFO)) {
-            String msg = formatter.format(format, arguments).toString();
-            log(SELF, Level.INFO, msg, null);
+            FormattingTuple tuple = MessageFormatter.arrayFormat(format, arguments);
+            log(SELF, Level.INFO, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void info(String format, Throwable throwable) {
         if (logger.isLoggable(Level.ALL)) {
-            String msg = formatter.format(format, throwable).toString();
-            log(SELF, Level.ALL, msg, throwable);
+            log(SELF, Level.ALL, format, throwable);
         }
     }
 
     @Override
     public void warn(String format, Object arg1) {
         if (logger.isLoggable(Level.WARNING)) {
-            String msg = formatter.format(format, arg1).toString();
-            log(SELF, Level.WARNING, msg, null);
+            FormattingTuple tuple = MessageFormatter.format(format, arg1);
+            log(SELF, Level.WARNING, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void warn(String format, Object arg1, Object obj2) {
         if (logger.isLoggable(Level.WARNING)) {
-            String msg = formatter.format(format, arg1, obj2).toString();
-            log(SELF, Level.WARNING, msg, null);
+            FormattingTuple tuple = MessageFormatter.format(format, arg1, obj2);
+            log(SELF, Level.WARNING, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void warn(String format, Object... arguments) {
         if (logger.isLoggable(Level.WARNING)) {
-            String msg = formatter.format(format, arguments).toString();
-            log(SELF, Level.WARNING, msg, null);
+            FormattingTuple tuple = MessageFormatter.arrayFormat(format, arguments);
+            log(SELF, Level.WARNING, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void warn(String format, Throwable throwable) {
         if (logger.isLoggable(Level.WARNING)) {
-            String msg = formatter.format(format, throwable).toString();
-            log(SELF, Level.WARNING, msg, throwable);
+            log(SELF, Level.WARNING, format, throwable);
         }
     }
 
     @Override
     public void error(String format, Object obj1) {
         if (logger.isLoggable(Level.SEVERE)) {
-            String msg = formatter.format(format, obj1).toString();
-            log(SELF, Level.SEVERE, msg, null);
+            FormattingTuple tuple = MessageFormatter.format(format, obj1);
+            log(SELF, Level.SEVERE, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void error(String format, Object obj1, Object obj2) {
         if (logger.isLoggable(Level.SEVERE)) {
-            String msg = formatter.format(format, obj1, obj2).toString();
-            log(SELF, Level.SEVERE, msg, null);
+            FormattingTuple tuple = MessageFormatter.format(format, obj1, obj2);
+            log(SELF, Level.SEVERE, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void error(String format, Object... arguments) {
         if (logger.isLoggable(Level.SEVERE)) {
-            String msg = formatter.format(format, arguments).toString();
-            log(SELF, Level.SEVERE, msg, null);
+            FormattingTuple tuple = MessageFormatter.arrayFormat(format, arguments);
+            log(SELF, Level.SEVERE, tuple.getMessage(), tuple.getThrowable());
         }
     }
 
     @Override
     public void error(String format, Throwable throwable) {
         if (logger.isLoggable(Level.SEVERE)) {
-            String msg = formatter.format(format, throwable).toString();
-            log(SELF, Level.SEVERE, msg, throwable);
+            log(SELF, Level.SEVERE, format, throwable);
         }
     }
 
