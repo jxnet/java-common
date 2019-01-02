14ea505d91efa0a171195f27cf307fa2392469ad
Ardika Rommy Sanjaya <ardikars@gmail.com>
2019-01-02 20:34:58 +0700

    Add read only iterator

---
+++ common-util/src/main/java/com/ardikars/common/util/AbstractReadOnlyIterator.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2017-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ardikars.common.util;
+
+/**
+ * Helper for jdk 1.2-
+ *
+ * @param <E> element type.
+ * @author <a href="mailto:contact@ardikars.com">Ardika Rommy Sanjaya</a>
+ * @since 1.2.6
+ */
+public abstract class AbstractReadOnlyIterator<E> implements CommonIterator<E> {
+
+    /**
+     * Remove method doesn't supported for read only iterator.
+     */
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("remove");
+    }
+
+}
