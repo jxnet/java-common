20dd54410366693b8cbcb081023246d77c696a89
Ardika Rommy Sanjaya <ardikars@gmail.com>
2019-01-05 09:24:11 +0700

    Update javadoc

--- common-annotation/src/main/java/com/ardikars/common/annotation/Incubating.java
+++ common-annotation/src/main/java/com/ardikars/common/annotation/Incubating.java
@@ -21,7 +21,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Indicates that a feature is incubating. This means that the feature is currently a work-in-progress and may
+ * Indicates that a feature is incubating or unstable. This means that the feature is currently a work-in-progress and may
  * change at any time.
  *
  * @author <a href="mailto:contact@ardikars.com">Ardika Rommy Sanjaya</a>
